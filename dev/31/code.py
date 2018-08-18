5bde3e5d72c45f480e06d55485f80eb882568ecc
bkface <42498364+bkface@users.noreply.github.com>
2018-08-19 01:58:37 +0800

    Create code.py

---
+++ dev/31/code.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/next-permutation/description/
+
+class Solution(object):
+    def nextPermutation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        
