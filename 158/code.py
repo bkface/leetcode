6211d6eb006b797fb9fdadd1e5744e8aae07ebc8
bkface <42498364+bkface@users.noreply.github.com>
2018-08-19 01:20:11 +0800

    Create code.py

---
+++ 158/code.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/read-n-characters-given-read4-ii-call-multiple-times/description/
+
+# The read4 API is already defined for you.
+# @param buf, a list of characters
+# @return an integer
+# def read4(buf):
+
+class Solution(object):
+    def read(self, buf, n):
+        """
+        :type buf: Destination buffer (List[str])
+        :type n: Maximum number of characters to read (int)
+        :rtype: The number of characters read (int)
+        """
+        
