b1ef69888a85b89f434eeeb6b4cabf3b41d7c179
bkface <42498364+bkface@users.noreply.github.com>
2018-08-19 01:12:46 +0800

    Create code.py

---
+++ 301/code.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/remove-invalid-parentheses/description/
+
+class Solution(object):
+    def removeInvalidParentheses(self, s):
+        """
+        :type s: str
+        :rtype: List[str]
+        """
+        
