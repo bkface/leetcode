7f882888c729b65d2f075656f36457dfd0a48c99
bkface <42498364+bkface@users.noreply.github.com>
2018-08-19 02:08:26 +0800

    Create code.py

---
+++ review/215/code.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/kth-largest-element-in-an-array/description/
+
+class Solution(object):
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        nums.sort(reverse=True)
+        return nums[k-1]
+        
