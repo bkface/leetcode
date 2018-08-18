224830de61e80d54abd3d7f6f9c991bf28e15c46
bkface <bkface28@gmail.com>
2018-08-19 02:51:04 +0800

    add attempted

---
+++ dev/647/code.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/palindromic-substrings/description/
+
+class Solution(object):
+    def countSubstrings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        
---
+++ dev/886/code.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/possible-bipartition/description/
+
+class Solution(object):
+    def possibleBipartition(self, N, dislikes):
+        """
+        :type N: int
+        :type dislikes: List[List[int]]
+        :rtype: bool
+        """
+        
